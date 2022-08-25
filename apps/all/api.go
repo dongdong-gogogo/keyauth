e2deff40e25a0b59fa3a003d9538686ec9e678d2
dongdong <1302525942@qq.com>
2022-08-25 14:35:11 +0800

    add keyauth

---
+++ apps/all/api.go
@@ -0,0 +1,6 @@
+package all
+
+import (
+	// 注册所有HTTP服务模块, 暴露给框架HTTP服务器加载
+	_ "gitee.com/dongdong-0421/keyauth/apps/book/api"
+)
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gitee.com/dongdong-0421/keyauth/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
